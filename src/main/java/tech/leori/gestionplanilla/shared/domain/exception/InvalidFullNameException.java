15c984620ea7d7a77d20e746f52f72d4b752ea7d
michel1n0 <81807063+michel1n0@users.noreply.github.com>
2025-11-02 10:32:02 -0500

    Add InvalidFullNameException domain exception

---
+++ src/main/java/tech/leori/gestionplanilla/shared/domain/exception/InvalidFullNameException.java
@@ -0,0 +1,7 @@
+package tech.leori.gestionplanilla.shared.domain.exception;
+
+public class InvalidFullNameException extends RuntimeException {
+    public InvalidFullNameException(String message) {
+        super(message);
+    }
+}
