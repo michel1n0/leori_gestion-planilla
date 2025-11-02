5a3a14f8565bfbd1118d8150ad93b9998bb22535
michel1n0 <81807063+michel1n0@users.noreply.github.com>
2025-11-02 18:05:42 -0500

    Refactor client factory to interface implementation

--- src/main/java/tech/leori/gestionplanilla/client/domain/factory/ClientFactory.java
+++ src/main/java/tech/leori/gestionplanilla/client/domain/factory/ClientFactory.java
@@ -2,16 +2,7 @@ package tech.leori.gestionplanilla.client.domain.factory;
 
 import tech.leori.gestionplanilla.client.domain.Client;
 
-public final class ClientFactory {
+public interface ClientFactory {
 
-    private ClientFactory() {
-    }
-
-    public static Client create(String businessName, String ruc, String industryType) {
-        return Client.builder()
-                .withBussinessName(businessName)
-                .withRuc(ruc)
-                .withIndustryType(industryType)
-                .build();
-    }
+    Client create(String name, String ruc, String address);
 }
