2d5cab1b89aefb58316127f87310d41a37a7c2a9
michel1n0 <81807063+michel1n0@users.noreply.github.com>
2025-11-02 17:13:45 -0500

    Add InvalidEmployeeAreaNameException domain exception

---
+++ src/main/java/tech/leori/gestionplanilla/shared/domain/exception/InvalidEmployeeAreaNameException.java
@@ -0,0 +1,7 @@
+package tech.leori.gestionplanilla.shared.domain.exception;
+
+public class InvalidEmployeeAreaNameException extends RuntimeException {
+    public InvalidEmployeeAreaNameException(String message) {
+        super(message);
+    }
+}
